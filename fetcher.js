ef461318eb9f1cb39ecfb3d6ff17e8c0e56bcdb1
Tyler Caceres <tyler.caceres@gmail.com>
2019-07-04 01:47:21 +0000

    fetcher finally works

--- fetcher.js
+++ fetcher.js
@@ -1,38 +1,64 @@
 const request = require("request");
 const fs = require("fs");
+const readline = require("readline");
 //argument 1 : URL
 //argument 2: local file path
 const args = process.argv.slice(2);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-let combine = function(name1, name2) {
-  let result = "";
-  name2 = name2.replace("./", "");
-  result = name1 + name2;
-  return result;
-};
-
-const URL = combine(args[0], args[1]);
-request(`${URL}`, (error, response, body) => {
-  console.log(URL);
+request(args[0], (error, response, body) => {
   if (error) {
-    console.log("cannot get request");
+    console.log("cannot get request - this is an error or non 200 result");
   } else {
-    //if not an error
-    fs.writeFile(args[1], body, error => {
-      if (error) {
-        console.log("could not create file");
-      } else {
-        fs.stat(args[1], (err, stats) => {
-          if (err) {
-            console.log("could not get file size");
+    fs.access(args[1], error => {
+      if (!error) {
+        //file does exist
+        //questions
+        rl.question("overide? press Y to overide.", answer => {
+          if (answer === "Y") {
+            //write file
+            fs.writeFile(args[1], body, error => {
+              if (error) {
+                console.log(
+                  "could not create file. local file path given is invalid"
+                );
+              } else {
+                //if file was created
+                console.log(
+                  `Downloaded and saved ${fs.statSync(args[1]).size} bytes to ${
+                    args[1]
+                  }`
+                );
+                //rl.close();
+              }
+            });
+            rl.close();
           } else {
+            rl.close();
+            //end
+          }
+        });
+      } else {
+        //if file does not exist
+        //if there is not an error - keep running the program
+        fs.writeFile(args[1], body, error => {
+          if (error) {
             console.log(
-              `Downloaded and saved ${stats.size} bytes to ${args[1]}`
+              "could not create file. local file path given is invalid"
+            );
+          } else {
+            //if file was created
+            console.log(
+              `Downloaded and saved ${fs.statSync(args[1]).size} bytes to ${
+                args[1]
+              }`
             );
           }
         });
       }
     });
   }
-  console.log("here");
 });
